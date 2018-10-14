bdffb1118aa00001206b898c1ce12246b2b33e44
moart <moart@LAPTOP-8O8DPEUF.Home>
2018-10-13 21:08:41 -0400

    created method, called method, used math method, and random class

--- IntegrationProject/src/Application.java
+++ IntegrationProject/src/Application.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 public class Application {
 
 	public static void main(String[] args) {
@@ -39,12 +40,55 @@ public class Application {
 		System.out.println(statementOne);
 		
 		//Variables are very important to java, they are a location in memory and are used to help code become flexible and manipulate values
-		// Scope refers to the section of code where a variable can be accessed
+		// Scope refers to the block of code belonging to a method following its declaration
 		
 		final double PI = 3.141592 ;
 		// Final is used to make whatever value it is assigned unchangeable or immutable
 	
 		System.out.println(PI);
+		
+		String name = "Stephen Alexander Aranda";
+		System.out.println(name.length());
+		// The string method  "length" is used to return the numbers of characters in a string as an integer
+		
+		String birthPlace = "I was born in Redwood City, ca";
+		String newbirthPlace = birthPlace.replaceFirst("ca","CA");
+		System.out.println(newbirthPlace);
+		// The replaceFirst method is used to replace the first occurrence only of a specific character in a string
+		
+		String hobby = "One of my favorite hobbies is to watch anime. Some examples are Naruto, My Hero Academia, Tokyo Ghoul, and Attack on Titan.";
+		String subOne = hobby.substring(0, 45);
+		String subTwo = hobby.substring(46, 123);
+		System.out.println(subOne + "\n" + subTwo);
+		// Substring methods returns a portion of a string from the index you choose
+		//The string above uses an escape sequence, which breaks the string into two lines
+		
+		int longToInt = (int)6400000L;
+		System.out.println(longToInt);
+		// I used casting in the code above to turn a larger data type which in this case is the long into a smaller one which here is an integer
+		
+		int n1 = 10, n2 = 20, maximum;
+		maximum = max(n1,n2); //Here the method is being called and in the parenthesis are the arguments
+		System.out.println("maximum is " + maximum);
+
+		int x1 = 100, randomNumber;
+		randomNumber = Methods.randomNum(x1);
+		double powerNum = Math.sqrt(randomNumber);
+		System.out.println("This is the sqare root of a random generated number from 0 to 100: " + powerNum );
+		// The block of code above uses a method that I created which generates a number
+		//from zero to 100 and then uses the square root method from the math class to take the square root of that random number and prints that value
+	}
+		public static int max(int number1, int number2) { //Here the method is being declared, also called the method header, and inside the parenthesis are the parameters
+			int result;
+			if (number1 > number2)
+				result = number1;
+			else 
+				result = number2;
+			return result;
+			
+			
+		
+
 	}
 
 }
---
+++ IntegrationProject/src/Methods.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+public class Methods {
+	public static int randomNum(int x) {
+		Random rndNum = new Random();
+		int rn = (int) rndNum.nextInt(x);
+		return rn;
+			
+	
+		
+	}
+
+}
